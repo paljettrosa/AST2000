88cc6735ea1f07ca4a392d969903db43fc202959
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-08 15:00:07 +0200

    Update f_fuel_consumption.py

--- f_fuel_consumption.py
+++ f_fuel_consumption.py
@@ -1,10 +1,10 @@
-import ast2000tools.constants as const
-
-def fuel_consumption(fuel_m, N_H2, N_box, time, spacecraft_m, mean_f, fuel_loss_s, delta_v):
-    m_H2 = const.m_H2                                       # mass of a H2 molecule [kg]
-    tot_init_m = spacecraft_m + fuel_m + m_H2*N_H2*N_box    # the rocket engine's initial mass with fuel included [kg]
-    thrust_f = N_box*mean_f/time                            # the combustion chamber's total thrust force [N]
-    a = thrust_f/tot_init_m                                 # the rocket's acceleration [m/s**2]
+def fuel_consumption(N_box, thrust_f, initial_m, fuel_loss_s, delta_v):
+    '''
+    we assume that the amount of fuel that the rocket loses during the speed boost
+    is so minimal that we can define the rocket's acceleration as the total thrust force
+    divided by it's total mass before the boost
+    '''
+    a = thrust_f/initial_m                                  # the rocket's acceleration [m/s**2]
     delta_t = delta_v/a                                     # time spent accelerating the rocket [s]
     tot_fuel_loss = abs(delta_t*fuel_loss_s*N_box)          # total fuel loss [kg]
     return tot_fuel_loss
