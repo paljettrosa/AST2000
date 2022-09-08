6bc504ad2ff9bd50013fdbc21923d0f72ed96feb
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-08 15:01:03 +0200

    Update f_absolute_velocity.py

--- f_absolute_velocity.py
+++ f_absolute_velocity.py
@@ -1,9 +1,14 @@
 import numpy as np
 from tqdm import trange
 
-def absolute_velocity(v, N):
-    abs_v = np.zeros(N) 
-    for i in trange(int(N)):
+'''
+made our own function to compute the absolute velocity for each particle
+and sort the values from smallest to largest, because it reduced runtime
+'''
+
+def absolute_velocity(v, N_H2):
+    abs_v = np.zeros(N_H2) 
+    for i in trange(int(N_H2)):
         for j in range(len(v[i])):
             abs_v[i] += v[i][j]**2
         abs_v[i] = np.sqrt(abs_v[i])
