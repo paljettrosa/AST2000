59c8c6bc432fb70ab4f325947d8d6adc245af234
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-08 03:19:01 +0200

    Update part1C.py

--- part1C.py
+++ part1C.py
@@ -1,30 +1,31 @@
 import numpy as np
 import ast2000tools.constants as const
 from tqdm import trange
+#from numba import njit
 
+#@njit
 def gasboxwnozzle(my, sigma, N, L, time, steps):
     r = np.random.uniform(0, L, size = (int(N), 3))         # position vector
     v = np.random.normal(my, sigma, size = (int(N), 3))     # velocity vector
+    m = const.m_H2                # mass of a H2 molecule [kg]
     dt = time/steps               # simulation step length [s]
     s = np.sqrt(0.25*L**2)        # length of all sides of the escape hole [m]
-    count = 0                     # amount of times one of the particles hit a wall
     exiting = 0                   # amount of particles that have exited the nozzle
-    f = 0                         # total force from escaped particles [N]
+    f = 0                         # total force from escaped particles [N]                  
     for i in trange(int(steps)):
         for j in range(int(N)):
             if s/2 <= r[j][0] <= 3*s/2 and s/2 <= r[j][1] <= 3*s/2 and r[j][2] <= 0:
-                exiting += 1
-                f += m*np.linalg.norm(v[j])/dt                          #riktig for kraften?
-                r[j] = (np.random.uniform(0, L, size = (1, 3)))         #riktig for å erstatte? tilfeldig pos?
-                v[j] = (np.random.normal(my, sigma, size = (1, 3)))
-            else:
-                for l in range(3):
-                    if r[j][l] <= 0 or r[j][l] >= L:
-                        count += 1
-                        v[j][l] = - v[j][l]
-        r += v*dt
-    cps = exiting*m/time          # fuel consumption per second [kg/s]
-    return r, v, count, exiting, f, cps
+                exiting += 1                                         # counting how many particles have exited the box
+                #f += m*np.linalg.norm(v[j])/dt                         
+                f += m*(- v[j][2])/dt                                # updating the box's thrust force
+                # spawning a new particle
+                r[j] = (np.random.uniform(0, L, size = (1, 3)))      # giving it a random position within the box
+                v[j] = (np.random.normal(my, sigma, size = (1, 3)))  # giving it a random velocity
+            for l in range(3):
+                if r[j][l] <= 0 or r[j][l] >= L:                     # checking if the particle hits one of the walls
+                     v[j][l] = - v[j][l]                             # bouncing the particle back
+        r += v*dt                                                    # updating the particles' velocities
+    return r, v, exiting, f
 
 N = 100                     # number of H_2 molecules
 
@@ -39,14 +40,20 @@ L = 10**(-6)                # length of sides in box [m]
 time = 10**(-9)             # time interval for simulation [s]
 steps = 1000                # number of steps taken in simulation
 
-r1, v1, count1, exiting1, f1, cps1 = gasboxwnozzle(my, sigma, N, L, time, steps)
+r, v, exiting, f = gasboxwnozzle(my, sigma, N, L, time, steps)
 
-print(r1)
-print(v1)
-print(count1)
-print(exiting1)
-print(f1)
-print(cps1)
+print(r)
+print(v)
+print(exiting)
+print(f)
+
+pp_s = exiting/time                 # the number of particles exiting per second [s**(-1)]
+mean_f = f/steps                    # the box force averaged over all time steps [N]
+fl_s = pp_s*m                       # the total fuel loss per second [kg/s]
+
+print(f'There are {pp_s:g} particles exiting the gas box per second')
+print(f'The gas box exerts a thrust of {mean_f:g} N')
+print(f'The box loses a mass of {fl_s:g} kg/s')
 
 '''
 SJEKK AT ALT BLIR RIKTIG / ALT ER GJORT
