83a38b93b07b34d8fa5332417a014a7e31655661
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-12 22:23:29 +0200

    Update f_gasboxwnozzle.py

--- f_gasboxwnozzle.py
+++ f_gasboxwnozzle.py
@@ -1,16 +1,16 @@
 import numpy as np
-from tqdm import trange
-import ast2000tools.constants as const
+from numba import jit
 
-def gasboxwnozzle(my, sigma, N, L, time, steps):
+@jit(nopython = True)
+def gasboxwnozzle(my, sigma, N, m, L, time, steps):
     r = np.random.uniform(0, L, size = (int(N), 3))         # position vector
     v = np.random.normal(my, sigma, size = (int(N), 3))     # velocity vector
-    m = const.m_H2                # mass of a H2 molecule [kg]
+    #m = const.m_H2                # mass of a H2 molecule [kg]
     dt = time/steps               # simulation step length [s]
     s = np.sqrt(0.25*L**2)        # length of all sides of the escape hole [m]
     exiting = 0                   # amount of particles that have exited the nozzle
     f = 0                         # total force from escaped particles [N]                  
-    for i in trange(int(steps)):
+    for i in range(int(steps)):
         for j in range(int(N)):
             if s/2 <= r[j][0] <= 3*s/2 and s/2 <= r[j][1] <= 3*s/2 and r[j][2] <= 0:
                 exiting += 1                                         # counting how many particles have exited the box
