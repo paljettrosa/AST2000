eb66931fa52bb86d23f3b7048fc47ebd9a0011c0
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-12 22:22:17 +0200

    Update part1B.py

--- part1B.py
+++ part1B.py
@@ -18,30 +18,29 @@ L = 10**(-6)                # length of sides in box [m]
 time = 10**(-9)             # time interval for simulation [s]
 steps = 1000                # number of steps taken in simulation
 
-r1, v1, count1 = gasbox(my, sigma, N_H2, L, time, steps)
+r, v, count = gasbox(my, sigma, N_H2, L, time, steps)
 
-print(r1)
-print(v1)
-print(count1)
+print(f'With {N_H2:g} H2-molecules in our gasbox, {count:g} molecules hit a wall in the box during the time interval of {time:g} s')
 
-abs_v1 = absolute_velocity(v1, N_H2)
+abs_v = absolute_velocity(v, N_H2)
 
-plt.plot(abs_v1, MaxwellBoltzmann_v(m_H2, k, T, abs_v1))
+plt.plot(abs_v, MaxwellBoltzmann_v(m_H2, k, T, abs_v))
 plt.show()
 
 N_H2 = 10**5               # number of H_2 molecules
 
-r2, v2, count2 = gasbox(my, sigma, N_H2, L, time, steps)
+r, v, count = gasbox(my, sigma, N_H2, L, time, steps)
 
-print(count2)
+print(f'With {N_H2:g} H2-molecules in our gasbox, {count:g} molecules hit a wall in the box during the time interval of {time:g} s')
 
-abs_v2 = absolute_velocity(v2, N_H2)
+abs_v = absolute_velocity(v, N_H2)
 
-plt.plot(abs_v2, MaxwellBoltzmann_v(m_H2, k, T, abs_v2))
+plt.plot(abs_v, MaxwellBoltzmann_v(m_H2, k, T, abs_v))
 plt.show()
 
-'''
-TESTE ACCURACY?? ENDRE TIDSINTERVALL??
+#TESTE ACCURACY???
 
-TA MED KJØREEKSEMPEL???
+'''
+With 100 H2-molecules in our gasbox, 883 molecules hit a wall in the box during the time interval of 1e-09 s
+With 100000 H2-molecules in our gasbox, 837006 molecules hit a wall in the box during the time interval of 1e-09 s
 '''
