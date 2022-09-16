40f7d257bd3f0844cba7c925dd1185974a9921fd
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-16 11:56:08 +0200

    Update part1D.py

--- part1D.py
+++ part1D.py
@@ -18,26 +18,26 @@ fuel_loss_s = particles_s*m_H2          # the total fuel loss per second [kg/s]
 
 A_box = L*L                             # area of one gasbox [m**2]
 A_spacecraft = mission.spacecraft_area  # area of our spacecraft's cross section [m**2]
-N_box = (A_spacecraft/A_box)*50         # number of gasboxes                   
-thrust_f = N_box*mean_f                 # the combustion chamber's total thrust force [N]
+N_box = (A_spacecraft/A_box)*20         # number of gasboxes                   
+thrust_f = N_box*mean_f                 # the combustion chamber's total thrust force per second [N]
 
 print(f'There are {particles_s*N_box:g} particles exiting the combustion chamber per second')
 print(f'The combustion chamber exerts a thrust of {thrust_f:g} N')
 print(f'The combustion chamber loses a mass of {fuel_loss_s*N_box:g} kg/s')
 
-spacecraft_m = mission.spacecraft_mass                  # mass of rocket without fuel [kg]
-fuel_m = 10**5                                          # mass of feul [kg]
-initial_m = spacecraft_m + fuel_m + m_H2*N_H2*N_box     # initial rocket mass [kg]
+spacecraft_m = mission.spacecraft_mass  # mass of rocket without fuel [kg]
+fuel_m = 1.5*10**4                      # mass of feul [kg]
+initial_m = spacecraft_m + fuel_m       # initial rocket mass [kg]
 
-delta_v = 10**4                                         # change in the rocket's velocity [m/s]
+delta_v = 10**4                         # change in the rocket's velocity [m/s]
 
 tot_fuel_loss = fuel_consumption(N_box, thrust_f, initial_m, fuel_loss_s, delta_v)
 
 print(f'The rocket uses a total of {tot_fuel_loss:g} kg fuel to boost its speed {delta_v:g} m/s')
 
 '''
-There are 2.81344e+28 particles exiting the combustion chamber per second
-The combustion chamber exerts a thrust of 389005 N
-The combustion chamber loses a mass of 94.1784 kg/s
-The rocket uses a total of 244764 kg fuel to boost its speed 10000 m/s
+There are 1.14544e+28 particles exiting the combustion chamber per second
+The combustion chamber exerts a thrust of 158225 N
+The combustion chamber loses a mass of 38.343 kg/s
+The rocket uses a total of 39015.5 kg fuel to boost its speed 10000 m/s
 '''
