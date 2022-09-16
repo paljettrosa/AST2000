767559f43347b9f7473f4b28f6feb3c5ef7c5151
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-16 11:48:31 +0200

    Update f_rocket_launch.py

--- f_rocket_launch.py
+++ f_rocket_launch.py
@@ -9,20 +9,29 @@ seed = utils.get_seed('hask')
 system = SolarSystem(seed)
 mission = SpaceMission(seed)
 
+x0 = utils.AU_to_m(system.initial_positions[0][0])                      # our planet's initial x-coordinate relative to our sun [m]
+y0 = utils.AU_to_m(system.initial_positions[1][0])                      # our planet's initial y-coordinate relative to our sun [m]
+
 def rocket_launch(r0, v0, v_esc, max_time, dt, thrust_f, initial_m, mass_loss_rate):
-    sim_launch_duration = 0                                                             # duration of our simulated rocket launch [s]
-    rocket_m = initial_m                                                                # the rocket's total mass [kg]
+    sim_launch_duration = 0             # duration of our simulated rocket launch [s]
+    rocket_m = initial_m                # the rocket's total mass [kg]
     N = max_time/dt                     # number of time steps
     r = np.zeros((int(N), 2))   
     v = np.zeros((int(N), 2))
     r[0] = r0                           # initial position [m]
     v[0] = v0                           # initial velocity [m/s]
     for i in range(int(N) - 1):
-        fG = gravity(r[i], rocket_m)                                    # the gravitational pull from our home planet [N]
+        '''
+        checking what referance system we're using
+        '''
+        if r[i][0] - x0 < 0:
+            fG = gravity(r[i], rocket_m)                                # the gravitational pull from our home planet [N]
+        elif r[i][0] - x0 >= 0:
+            fG = gravity(r[i] - np.array([x0, y0]), rocket_m)           # the gravitational pull from our home planet [N]
         a = np.array([(thrust_f + fG[0])/rocket_m, fG[1]/rocket_m])     # the rocket's total acceleration at current time step [m/s**2]
         v[i+1] = v[i] + a*dt                                            # updated velocity
         r[i+1] = r[i] + v[i+1]*dt                                       # updated position
-        rocket_m -= mass_loss_rate                                      # updating the rocket's mass during the launch
+        rocket_m -= mass_loss_rate*dt                                   # updating the rocket's mass during the launch
         if thrust_f <= np.linalg.norm(fG):                              # checking if the thrust force is too low       
             print('Thrust force is too low!')
             break
