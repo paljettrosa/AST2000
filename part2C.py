b13331649e2c7919c25662980308d5e9710a8ebb
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-09-20 19:45:40 +0200

    Update part2C.py

--- part2C.py
+++ part2C.py
@@ -41,10 +41,13 @@ init_pos = system.initial_positions
 init_vel = system.initial_velocities
 
 F_r0 = np.array([init_pos[0][4], init_pos[1][4]])   # Flora's initial position [AU]
-F_v0 = np.array([init_vel[0][4], init_vel[1][4]])   # Flora's initial velocity [AU/yr]          
+F_v0 = np.array([init_vel[0][4], init_vel[1][4]])   # Flora's initial velocity [AU/yr]
 
-sun_r0 = np.array([0.0, 0.0])                       # our sun's initial position
-sun_v0 = np.array([0.0, 0.0])                       # our sun's initial velocity
+sun_r0 = np.array([0.0, 0.0])                       # our sun's initial position [AU]
+sun_v0 = - F_v0*m/M                                 # our sun's initial velocity [AU/yr]
+
+cm_r = M/(m + M)*sun_r0 + m/(m + M)*F_r0            # center of mass position
+cm_v = M/(m + M)*sun_v0 + m/(m + M)*F_v0            # center of mass velocity
 
 '''
 changing frame of reference: cm_r is the positional vector of the center of
@@ -53,11 +56,9 @@ origin of our current frame of reference, the positional vector points from
 our sun to the center of mass
 '''
 
-cm_r = M/(m + M)*sun_r0 + m/(m + M)*F_r0            # center of mass position
-cm_v = M/(m + M)*sun_v0 + m/(m + M)*F_v0            # center of mass velocity
-
 F_r0 = F_r0 - cm_r                                  # Flora's initial position relative to the center of mass
 F_v0 = F_v0 - cm_v                                  # Flora's initial velocity relative to the center of mass
+
 sun_r0 = sun_r0 - cm_r                              # our sun's initial position relative to the center of mass
 sun_v0 = sun_v0 - cm_v                              # our sun's initial velocity relative to the center of mass
 
@@ -65,39 +66,46 @@ N = 10*10**4        # amount of time steps
 dt = 10*P/N         # time step
 
 #@jit(nopython = True)
-def simulate_orbits(N, m, M, planet_r0, planet_v0, sun_r0, sun_v0):
+def simulate_orbits(N, dt, m, M, planet_r0, planet_v0, star_r0, star_v0):
     G = 4*np.pi**2                          # gravitation constant [AU**3yr**(-2)M**(-1)]
     r = np.zeros((N, 2, 2))
     v = np.zeros((N, 2, 2))
-    r[0] = np.array([planet_r0, sun_r0])
-    v[0] = np.array([planet_v0, sun_v0])
-    mM = np.array([m, M])
+    r[0] = np.array([planet_r0, star_r0])
+    v[0] = np.array([planet_v0, star_v0])
     for i in range(N - 1):
-        for j in range(2):
-            '''
-            R = np.array([r[i][0] - r[i][1], r[i][1] - r[i][0]])        # planet's and sun' position relative to each other
-            d = np.linalg.norm(R[j])                                    # the distance between the planet and the sun
-            g = - G*m*M/d**3*R[j]
-            '''
-            g = - G*mM[j]/np.linalg.norm(r[i][j])**3*r[i][j]
-            v[i+1][j] = v[i][j] + g*dt/2
-            r[i+1][j] = r[i][j] + v[i+1][j]*dt
-            '''
-            R = np.array([r[i+1][0] - r[i+1][1], r[i+1][1] - r[i+1][0]])        
-            d = np.linalg.norm(R[0])         
-            g = - G*m*M/d**3*R[j]
-            '''
-            g = - G*mM[j]/np.linalg.norm(r[i+1][j])**3*r[i+1][j]
-            v[i+1][j] = v[i+1][j] + g*dt/2
+        R = r[i, 0] - r[i, 1]
+        g_planet = - G*M/np.linalg.norm(R)**3*R
+        g_star = - G*m/np.linalg.norm(R)**3*(- R)
+        
+        v[i+1, 0] = v[i, 0] + g_planet*dt/2
+        v[i+1, 1] = v[i, 1] + g_star*dt/2
+        
+        r[i+1, 0] = r[i, 0] + v[i+1, 0]*dt
+        r[i+1, 1] = r[i, 1] + v[i+1, 1]*dt
+        
+        R = r[i+1, 0] - r[i+1, 1]
+        g_planet = - G*M/np.linalg.norm(R)**3*R
+        g_star = - G*m/np.linalg.norm(R)**3*(- R)
+        
+        v[i+1, 0] = v[i+1, 0] + g_planet*dt/2
+        v[i+1, 1] = v[i+1, 1] + g_star*dt/2
     return r, v
 
-r, v = simulate_orbits(N, m, M, F_r0, F_v0, sun_r0, sun_v0)
+r, v = simulate_orbits(N, dt, m, M, F_r0, F_v0, sun_r0, sun_v0)
+
+planet_sun = np.array([['Flora', 'pink'], ['Sun', 'orange']])
 
 for i in range(2):
-    plt.plot(r[:, i, 0], r[:, i, 1])
+    plt.plot(r[:, i, 0], r[:, i, 1], color = planet_sun[i][1], label = planet_sun[i][0])
+plt.legend()
+plt.title("Flora's and our sun's orbit around their center of mass")
 plt.show()
 
-plt.plot(r[:, 0, 0], r[:, 0, 1])
+plt.plot(r[:, 1, 0], r[:, 1, 1], color = 'orange')
+plt.title("Our sun's orbit around the center of mass")
 plt.show()
 
-#HVA I HELVETE GJØR MAN HER
+'''
+the center of mass is in the origin, so we see that both Flora and the sun has
+elliptical orbits bith the center of mass in one of the focal points
+'''
