a3b4866ac4eeff72de9451079075d2d90845b3bc
paljettrosa <112864944+paljettrosa@users.noreply.github.com>
2022-10-05 04:20:56 +0200

    Add files via upload

--- part4A.py
+++ part4A.py
@@ -1,7 +1,19 @@
 #EGEN KODE
-from libraries import *
+import numpy as np
 from PIL import Image
 import imageio as ig
+import ast2000tools.utils as utils
+from ast2000tools.solar_system import SolarSystem
+from ast2000tools.space_mission import SpaceMission
+
+utils.check_for_newer_version()
+seed = utils.get_seed('somiamc')
+system = SolarSystem(seed)
+mission = SpaceMission(seed)
+
+planets = np.array([['Doofenshmirtz', 'black'], ['Blossom', 'crimson'], 
+                    ['Bubbles', 'skyblue'], ['Buttercup', 'olivedrab'], 
+                    ['Flora', 'pink'], ['Stella', 'gold'], ['Aisha', 'darkorchid']])
 
 '''
 A. Generating Reference Pictures
